97ee404191360ad89481b44198e058baed72731b
Vincent Neang <vtneang@gmail.com>
2020-04-21 21:56:01 -0700

    Title/End Scenes kinda done?
    Added some of the Ui for title/end screen

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string nextScene;
+    public string LevelSelection;
+    #region Play Buttons Methods
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene(LevelSelection);
+    }
+    #endregion
+
+    #region General Application Button Methods
+    public void Quit()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+    #endregion
+}
